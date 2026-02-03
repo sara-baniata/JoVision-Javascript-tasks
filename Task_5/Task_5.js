03f0531916c76e7dbe6d1ab3027ca6e0ec0e4b87
sara-baniata <sarahbaniatta@gmail.com>
2026-02-03 15:36:12 +0300

    Add task_5 and buttonStyle.css and task_5.js files

--- Task_5/Task_5.js
+++ Task_5/Task_5.js
@@ -6,10 +6,18 @@ function task4(){
 	let  arr=[];
 	for (let i=0;i<=100;i++){
 		arr.push(i);
-		if(i%2==0)
-	    sum+=i;
+		//if (i%2==0)
+	    //sum+=i;
+	    //another method by filter 
 	}
-    
+	let arr2=arr.filter(fun_even);
+	for (let i=0;i<arr2.length;i++)
+		sum+=arr2[i];
+
 	document.getElementById('SA').innerHTML="<strong style='color:#F54927'>The sum of the array elements is : </strong>"+sum;
 	
+}
+function fun_even(value)
+{   
+	return value%2==0;
 }
