ff17a901995c7a2178405b2110859019ba9753e5
sara-baniata <sarahbaniatta@gmail.com>
2026-02-03 14:49:15 +0300

    Add task_4 and buttonStyle.css and task_4.js files

---
+++ Task_4/task4.js
@@ -0,0 +1,13 @@
+
+
+
+function task4(){
+    let sum=0;
+	let  arr=[];
+	for (let i=0;i<=100;i++){
+		arr.push(i);
+	    sum+=i;
+	}
+	document.getElementById('SA').innerHTML="<strong style='color:#F54927'>The sum of the array elements is : </strong>"+sum;
+	
+}
