e68b64a1f33824848bef1ed4a7f32aad0f8f8a64
sara-baniata <sarahbaniatta@gmail.com>
2026-02-04 16:59:15 +0300

    add task8 and its files

---
+++ Task8/httpRequest.js
@@ -0,0 +1,22 @@
+
+async function fun(){
+ try{
+ const response=await fetch('https://api.ipify.org/');
+ if(response.ok){
+ const data= await response.text();
+ console.log(data);
+ }
+ else{
+ throw new Error('Failed to fetch data');
+ }
+}
+catch (error){console.error('Error:', error);}
+
+}
+/*
+function fun(){
+  fetch('https://api.ipify.org/')
+      .then(response => response.text())
+      .then (data => console.log(data))
+      .catch(error =>console.error('Error:', error));
+}*/
